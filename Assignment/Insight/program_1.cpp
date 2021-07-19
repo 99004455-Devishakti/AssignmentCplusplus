da05c553a8e449fa362bc874a736f303d503b409
Devishakti Jyotishankar Behera <84493238+99004455-Devishakti@users.noreply.github.com>
2021-07-19 22:19:44 +0530

    Updated From Main

--- Assignment/Insight/program_1.cpp
+++ Assignment/Insight/program_1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-class box
+class Box
 {
 private:   
     int m_length;
@@ -10,82 +10,82 @@ private:
    
 public:
     /* Constructors */
-    box();
-    box(int val);
-    box(int arr[]);
-    box(int lenth, int breadth, int height);
-    box(const box& ref);
+    Box();
+    Box(int val);
+    Box(int arr[]);
+    Box(int lenth, int breadth, int height);
+    Box(const Box& ref);
 
     /* Getter function */
     int volume() const;
     int get_length() const;
     int get_breadth()const;
     int get_height()const;
-    box* construct(int index);
+    Box* construct(int index);
 
     /* destructors */
-    ~box();
+    ~Box();
 };
-box::box(/* args */):m_length(0.0), m_breadth(0.0), m_height(0.0), m_volume(0.0) {}
+Box::Box(/* args */):m_length(0.0), m_breadth(0.0), m_height(0.0), m_volume(0.0) {}
 
-box::box(int val):m_length(val), m_breadth(val), m_height(val), m_volume(val * val * val) {}
-box::box(int length, int breadth, int height):m_length(length), m_breadth(breadth), m_height(height), m_volume(length * breadth * height) {}
-box::box(int arr[]):m_length(arr[0]), m_breadth(arr[1]), m_height(arr[2]), m_volume(arr[0]*arr[1]*arr[2]) { }
-box::box(const box& ref):m_length(ref.m_length), m_breadth(ref.m_breadth), m_height(ref.m_height), m_volume(ref.m_length * ref.m_height * ref.m_breadth) {}
+Box::Box(int val):m_length(val), m_breadth(val), m_height(val), m_volume(val * val * val) {}
+Box::Box(int length, int breadth, int height):m_length(length), m_breadth(breadth), m_height(height), m_volume(length * breadth * height) {}
+/* Array Constructors */
+Box::Box(int arr[]):m_length(arr[0]), m_breadth(arr[1]), m_height(arr[2]), m_volume(arr[0]*arr[1]*arr[2]) { }
+Box::Box(const Box& ref):m_length(ref.m_length), m_breadth(ref.m_breadth), m_height(ref.m_height), m_volume(ref.m_length * ref.m_height * ref.m_breadth) {}
 
-box::~box() {}
-int box::volume() const{
+Box::~Box() {}
+int Box::volume() const{
     return m_length * m_breadth * m_height;
 }
 
-int box::get_length() const{
+int Box::get_length() const{
     return m_length;
 }
 
 
-int box::get_breadth() const{
+int Box::get_breadth() const{
     return m_breadth;
 }
 
-int box::get_height() const{
+int Box::get_height() const{
     return m_height;
 }
 
-box* construct(int index, box* p, int size){
-    int count=0;
-    while(count<size){
-        if(count==index){
-            box ab(20,14,36);
+Box* construct(int index, Box* p, int s){
+    int c=0;
+    while(c<s){
+        if(c==index){
+            Box ab(20,14,36);
             *p=ab;
             return p;          
         }
-        count++;
+        c++;
         p++;
     }
     return nullptr;
 }
 
 int main(){
-    int arr[]={5,12,13};
-    box b(arr);
+    int arr1[]={5,12,13};
+    Box bx(arr1);
     int n=5;
     int arr2[n][3];
-    box arr3[n];
+    Box arr3[n];
     int val=5;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < 3; j++)
         {
             arr2[i][j]=val++;
-        }
-        
+        }  
     }
     for (int i = 0; i < n; i++)
     {
-        arr3[i]=box(arr2[i]);
+        arr3[i]=Box(arr2[i]);
     }
-    box arr4[n];
-    box *ptr=arr4;
-
-    box *temptr=construct(2, ptr, n);
+    Box arr4[n];
+    Box *ptr=arr4;
+    Box *temp=construct(2, ptr, n);
+    return 0;
 }
